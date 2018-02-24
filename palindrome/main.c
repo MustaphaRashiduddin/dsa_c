8fab57b33e56f7d80274664141d49afd72f4fde8
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2018-02-24 13:51:17 +0000

    palindrome algorithm

---
+++ palindrome/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+        if (argc != 2) {
+                printf("supply ONE string argument\n");
+                return 1;
+        }
+
+        char *str = argv[1];
+        int n = strlen(str);
+        int pal = 1;
+
+        for (int i=0; i<n; i++)
+                if (str[i] != str[n-i-1]) {
+                        pal = 0;
+                        break;
+                }
+
+        pal == 1 ? n : printf("not ");
+        printf("palindrome\n");
+                
+        return 0;
+}
