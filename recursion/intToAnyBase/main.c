e0f0b08f98ee8c52e401cd7410c4d299b3b56262
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2018-03-20 23:10:09 +0000

    fun return str now

--- recursion/intToAnyBase/main.c
+++ recursion/intToAnyBase/main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void convert(int n, int b, char *str);
+char *convert(int n, int b, char *str);
 
 int main(int argc, char **argv)
 {
         char str[100];
-        convert(10, 2, str);
-        /* convert(769, 10, str); */
+        /* convert(10, 2, str); */
+        convert(769, 10, str);
         printf("%s", str);
 
         return 0;
 }
 
-void convert(int n, int b, char *str)
+char *convert(int n, int b, char *str)
 {
         if (n<b) {
                 sprintf(str, "%d", n%b);
-                return;
+                return str;
         }
         sprintf(str, "%d", n%b);
-        convert(n/b, b, str+1);
+        char *sub = convert(n/b, b, str+1);
+        return sub;
 }
