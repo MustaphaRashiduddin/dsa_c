139d42e700b3bc784a1d8fd4f6e01d250d4a9ede
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2018-03-23 22:17:40 +0000

    exact integers malloced to str before hand

--- recursion/intToAnyBase/main.c
+++ recursion/intToAnyBase/main.c
@@ -6,13 +6,13 @@ void convert(int n, int b, char *str, int f, int l);
 
 int main(int argc, char **argv)
 {
-        char str[100];
         int n = 25;
         int b = 2;
         int l = ceil(log(n)/log(b));
+        char *str = malloc(l+1);
         convert(n, b, str, 0, l);
         printf("%s", str);
-
+        free(str);
         return 0;
 }
 
