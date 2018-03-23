24ca94ff6ff93aa73cc8ddbaff99c61c6b816562
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2018-03-23 22:51:19 +0000

    handles upto hexedecimal

--- recursion/intToAnyBase/main.c
+++ recursion/intToAnyBase/main.c
@@ -2,26 +2,38 @@
 #include <stdlib.h>
 #include <math.h>
 
-void convert(int n, int b, char *str, int f, int l);
+// returns malloced (char *)
+char *convert(int n, int b);
+void convert_aux(int n, int b, char *str, int f, int l);
+
+const char *nums = {"0123456789ABCDEF"};
 
 int main(int argc, char **argv)
 {
-        int n = 25;
-        int b = 2;
-        int l = ceil(log(n)/log(b));
-        char *str = malloc(l+1);
-        convert(n, b, str, 0, l);
-        printf("%s", str);
+        int n = 1453;
+        int b = 16;
+        
+        char *str = convert(n, b);
+
+        printf("%s\n", str);
         free(str);
         return 0;
 }
 
-void convert(int n, int b, char *str, int i, int l)
+char *convert(int n, int b)
+{
+        int l = ceil(log(n)/log(b));
+        char *str = malloc(l+1);
+        convert_aux(n, b, str, 0, l);
+        return str;
+}
+
+void convert_aux(int n, int b, char *str, int i, int l)
 {
         if (n<b) {
-                sprintf(str, "%d", n%b);
+                sprintf(str, "%c", nums[n%b]);
                 return;
         }
-        convert(n/b, b, str, ++i, l);
-        sprintf(str+(l-i), "%d", n%b);
+        convert_aux(n/b, b, str, ++i, l);
+        sprintf(str+(l-i), "%c", nums[n%b]);
 }
